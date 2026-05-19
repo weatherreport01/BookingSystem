64b5cec02be34a4c7b2c51be8f94f16d5e269121
jb <vagabigben@tutanota.com>
2026-05-19 17:22:50 +0100

    Added repo annotations

--- src/main/java/com/jb/bookingSystem/persistence/repository/BookingRepository.java
+++ src/main/java/com/jb/bookingSystem/persistence/repository/BookingRepository.java
@@ -3,9 +3,11 @@ package com.jb.bookingSystem.persistence.repository;
 import com.jb.bookingSystem.persistence.entity.BookingEntity;
 import com.jb.bookingSystem.persistence.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface BookingRepository extends JpaRepository<BookingEntity,Integer> {
     Optional<BookingEntity> findByMemberName(String name);
 }
--- src/main/java/com/jb/bookingSystem/persistence/repository/RoomRepository.java
+++ src/main/java/com/jb/bookingSystem/persistence/repository/RoomRepository.java
@@ -4,10 +4,12 @@ import com.jb.bookingSystem.persistence.entity.MemberEntity;
 import com.jb.bookingSystem.persistence.entity.RoomEntity;
 import com.jb.bookingSystem.persistence.entity.RoomType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface RoomRepository extends JpaRepository<RoomEntity,Integer> {
     Optional<RoomEntity> findByRoomNumber(int roomNumber);
 
